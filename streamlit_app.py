e35a8d93e6a29e9a3daf19780df235b6f6223a7c
William Schmitt <9754116+spicysouvlaki@users.noreply.github.com>
2021-10-26 14:51:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 import streamlit as st
 import os
 
+st.write("hello")
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
